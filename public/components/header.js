9255401ca70905b46bfd89a996e3252c791f408e
Jon Gilberg <j.gilberg@outlook.com>
2025-05-19 01:26:14 -0700

    Adjusted colours

--- public/components/header.js
+++ public/components/header.js
@@ -12,7 +12,7 @@ class SiteHeader extends HTMLElement {
 
             <a
                 href="/login"
-                class="px-4 py-2 text-sm font-medium text-white rounded-full bg-violet-600 hover:bg-violet-500 transition"
+                class="px-4 py-2 text-sm font-medium text-white rounded-full bg-violet-600 hover:bg-violet-700 transition"
             >
                 Log in
             </a>
